f59b9006526f5d6753e64b9f185a64bfe239a5a6
Tseng <53459541+tsengwaiyong@users.noreply.github.com>
2021-03-07 01:12:00 +0800

    Add files via upload

--- js/MousePointer.js
+++ js/MousePointer.js
@@ -1,5 +1,41 @@
+var card_m = $(".card-m");
+var card_l = $(".card-l");
+var mouse = $(".mousepointer");
+
 $(document).mousemove(function(e) {
 
-  $('.mousepointer').css("left", e.pageX);
-  $('.mousepointer').css("top", e.pageY);
+  mouse.css({
+    "left": e.pageX,
+    "top": e.pageY
+  });
+});
+
+card_m.mouseover(function(e) {
+  mouse.css("opacity", "0");
+
+  $(e.target).mousemove(function(e) {
+    var offsetx = e.offsetX / 20 + 'px'
+    var offsety = e.offsetY / 20 + 'px'
+
+    $(e.target).css({
+      "transform": "translate(" + offsetx + "," + offsety + ")"
+    });
+  });
+
+  $(e.target).mouseleave(function(e) {
+    mouse.css("opacity", "1");
+    $(e.target).css({
+      "transform": "translate(0px,0px)"
+
+    });
+  });
+});
+
+
+card_l.mouseover(function() {
+  mouse.css("opacity", "0");
+});
+
+card_l.mouseleave(function() {
+  mouse.css("opacity", "1");
 });
